d8b940518e0c02e101fafa57fbc4bea856ca972a
diyaprajapati <diyabprajapati2005@gmail.com>
2024-08-07 21:07:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-cyan-400 font-bold'>hello</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

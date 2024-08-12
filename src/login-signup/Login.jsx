ef3853ae000a5135f69473da937cb5be161adcbe
diyaprajapati <diyabprajapati2005@gmail.com>
2024-08-12 21:50:18 +0530

    make onClick and useStates to store data

--- src/login-signup/Login.jsx
+++ src/login-signup/Login.jsx
@@ -1,42 +1,114 @@
-import React from 'react';
-// import axios from 'axios'
+import React, {useState} from 'react';
+import axios from 'axios'
 import {Link} from 'react-router-dom';
 import Button from '../Button';
 
 export default function Login() {
-  return (
-    <div className='flex justify-center items-center h-screen'>
-    <form className='w-full h-full'>
-        <div className='flex flex-col justify-center items-center h-full'>
 
-            {/* Label */}
-            <h1 className='text-3xl p-6 font-semibold'>Log-In</h1>
+    const [popup, setPopup] = useState({
+        visible: false,
+        message: '',
+        success: false
+    });
 
-            {/* email */}
-            <div className='flex flex-col p-4 gap-3 w-80'>
-                <div className='flex flex-col space-y-2'>
-                    <label className='font-medium text-left'>E-mail:</label>
-                    <input className='border-2 py-2 px-8 rounded-md focus:outline-none border-gray-400' type='email' placeholder='xyz@gmail.com'/>
+    const [loginData, setLoginData] = useState({
+        email: '',
+        password: ''
+    });
+
+    // submit function
+    const handleLoginSubmit = async(e) => {
+        e.preventDefault();
+
+        // popup msg
+        setPopup({visible: true, message: 'Login Successfully!', success:true});
+
+        try {
+            const response = await axios.post('http://localhost:3000/', loginData);
+
+            if(popup.success) {
+                console.log('Login Successful: ', popup.success, popup.message);
+            }
+            else {
+                console.log('Login Failed: ', popup.success, popup.message);
+            }
+        }
+        catch(error) {
+            setPopup({visible:true, message: 'Invalid email or password', success: false});
+            console.log('Login error', error);
+        }        
+        setLoginData({
+            email: '',
+            password: ''
+        });
+
+        // hide popup after 3 sec
+        setTimeout(() => {
+            setPopup((prevPopup) => ({...prevPopup, visible: false}));
+        }, 3000);
+    }
+
+    const handleLoginChange = (e) => {
+        const {name, value} = e.target;
+        setLoginData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    }
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <form className='w-full h-full' onSubmit={handleLoginSubmit}>
+                <div className='flex flex-col justify-center items-center h-full'>
+                    
+                    {/* Label */}
+                    <h1 className='text-3xl p-6 font-semibold'>Log-In</h1>
+
+                    {/* email */}
+                    <div className='flex flex-col p-4 gap-3 w-80'>
+                        <div className='flex flex-col space-y-2'>
+                            <label className='font-medium text-left'>E-mail:</label>
+                            <input 
+                                className='border-2 py-2 px-8 rounded-md focus:outline-none border-gray-400' 
+                                type='email' 
+                                placeholder='xyz@gmail.com' 
+                                name='email'
+                                value={loginData.email} 
+                                onChange={handleLoginChange} 
+                                required
+                            />
+                        </div>
+
+                        {/* password */}
+                        <div className='flex flex-col space-y-2'>
+                            <label className='font-medium text-left'>Password:</label>
+                            <input 
+                                className='border-2 py-2 px-8 rounded-md focus:outline-none border-gray-400' 
+                                type='password' 
+                                placeholder='password' 
+                                name='password'
+                                value={loginData.password} 
+                                onChange={handleLoginChange} 
+                                required 
+                            />
+                        </div>
+                    </div>
+
+                    {/* Button */}
+                    <Button>Log-in</Button>
+                    {popup.visible && (
+                <div className={`p-5 rounded-md shadow-lg ${popup.success ? 'bg-green-200 text-green-800 border-green-600' : 'bg-red-200 text-red-800 border-red-600'}`}>
+                    <p>{popup.message}</p>
                 </div>
+            )}
 
-                {/* password */}
-                <div className='flex flex-col space-y-2'>
-                    <label className='font-medium text-left'>Password:</label>
-                    <input className='border-2 py-2 px-8 rounded-md focus:outline-none border-gray-400' type='password' placeholder='password'/>
+                    {/* signup if doesn't login */}
+                    <div className='flex gap-4 mt-3'>
+                        <p>Don't have an account?</p>
+                        <Link className='font-normal underline hover:font-medium' to='/signup'> Signup </Link>
+                    </div>
                 </div>
-
-            </div>
-
-            {/* Button */}
-                <Button>Log-in</Button>
-
-            {/* signup if doesn't login */}
-            <div className='flex gap-4 mt-3'>
-                <p>Don't have an account?</p>
-                <Link className='font-normal underline hover:font-medium' to='/signup'> Signup </Link>
-            </div>
+            </form>
         </div>
-    </form>
-    </div>
-  )
+    )
 }
